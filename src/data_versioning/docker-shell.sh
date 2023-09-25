ae6552073c385f601b827f1277315483b7432925
Kyle (Si yang) Ke <siyangke98@gmail.com>
2023-09-25 16:04:29 -0400

    data version container

--- src/data_versioning/docker-shell.sh
+++ src/data_versioning/docker-shell.sh
@@ -6,17 +6,17 @@ export BASE_DIR=$(pwd)
 export GCP_PROJECT="AC215"
 export GCP_ZONE="us-central1-a"
 export GCS_BUCKET_NAME="fakenew_classifier_data_bucket"
-
+ABSOLUTE_PATH=$(realpath ../../.git)
 # Create the network if we don't have it yet
 docker network inspect data-versioning-network >/dev/null 2>&1 || docker network create data-versioning-network
 
 # Build the image based on the Dockerfile
-docker build -t data-version-cli --platform=linux/arm64/v8 -f Dockerfile .
+docker build -t data-version-cli --platform=linux/amd64 -f Dockerfile .
 
 # Run Container
 docker run --rm --name data-version-cli -ti \
 -v "$BASE_DIR":/app \
--v ~/.gitconfig:/etc/gitconfig \
+-v "$ABSOLUTE_PATH":/app/.git \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCP_ZONE=$GCP_ZONE \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
