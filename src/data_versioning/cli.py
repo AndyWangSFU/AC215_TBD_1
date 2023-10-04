ff1afb9352082f54dffedf537efebbfd656a1d77
Boshen Yan <boshen@live.com>
2023-10-04 13:36:26 -0400

    Update data versioning

--- src/data_versioning/cli.py
+++ src/data_versioning/cli.py
@@ -10,67 +10,30 @@ import shutil
 import glob
 import json
 
+GCP_PROJECT = "AC215"
+BUCKET_NAME = "fakenew_classifier_data_bucket"
 
-GCS_BUCKET_NAME = "fakenew_classifier_data_bucket"
 
+def download_data(filepath="cleaned_metadata"):
+    storage_client = storage.Client(project=GCP_PROJECT)
 
-def download_data():
-    print("download_data")
-
-    bucket_name = GCS_BUCKET_NAME
-
-    # Clear dataset folders
-    dataset_prep_folder = "image_dataset_prep"
-    shutil.rmtree(dataset_prep_folder, ignore_errors=True, onerror=None)
-    os.makedirs(dataset_prep_folder, exist_ok=True)
-    dataset_folder = "image_dataset"
-    shutil.rmtree(dataset_folder, ignore_errors=True, onerror=None)
-    os.makedirs(dataset_folder, exist_ok=True)
-
-    # Initiate Storage client
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blobs = bucket.list_blobs(prefix="raw_images/public_image_set")
-
-    # Download annotations
+    # Get reference to bucket
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blobs = bucket.list_blobs(prefix=filepath)
     for blob in blobs:
-        print("Annotation file:", blob.name)
-
-        filename = os.path.basename(blob.name)
-        local_file_path = os.path.join(dataset_prep_folder, filename)
-        blob.download_to_filename(local_file_path)
-
-    # Organize annotation with images
-    annotation_files = glob.glob(os.path.join(dataset_prep_folder, "*"))
-    for annotation_file in annotation_files:
-        # Read the json file
-        with open(annotation_file, "r") as read_file:
-            annotation_json = json.load(read_file)
-
-        # Annotations
-        # annotations = annotation_json["annotations"]
-        # Assume we pick just the first annotation from the labeled list
-        if len(annotation_json["result"]) > 0:
-            label = annotation_json["result"][0]["value"]["choices"][0]
-            # Create the label folder
-            label_folder = os.path.join(dataset_folder, label)
-            os.makedirs(label_folder, exist_ok=True)
-
-            # Download the image from GCS [Another option could be to just store the image url and label in DVC]
-            image_url = annotation_json["task"]["data"]["image"]
-            image_url = image_url.replace("gs://", "").replace(
-                GCS_BUCKET_NAME + "/", ""
-            )
-            print("image_url:", image_url)
-            blob = bucket.blob(image_url)
-            filename = os.path.basename(blob.name)
-            local_file_path = os.path.join(label_folder, filename)
-            blob.download_to_filename(local_file_path)
+        if not blob.name.endswith("/"):
+            try:
+            # Download the blob to a local file with the same name
+                blob.download_to_filename(blob.name)
+                print(f"Downloaded: {blob.name}")
+            except Exception as e:
+                print(f"Error downloading {blob.name}: {str(e)}")
 
 
 def main(args=None):
     if args.download:
-        download_data()
+        download_data(args.me)
+    
 
 
 if __name__ == "__main__":
@@ -83,6 +46,12 @@ if __name__ == "__main__":
         "--download",
         action="store_true",
         help="Download labeled data from a GCS Bucket",
+    )
+    parser.add_argument(
+        "-f",
+        "--filepath",
+        type=str,
+        default="cleaned_metadata",
     )
 
     args = parser.parse_args()
