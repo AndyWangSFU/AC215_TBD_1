b36c3c03d01c806545932a2627520f47c351bb7d
root <root@inst-builder-debian-11-build-build-250sr.us-central1-a.c.gce-image-builder.internal>
2023-12-07 02:57:50 +0000

    Setup k8s

--- src/deployment/docker-shell.sh
+++ src/deployment/docker-shell.sh
@@ -22,7 +22,7 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$SECRETS_DIR":/secrets \
 -v "$HOME/.ssh":/home/app/.ssh \
 -v "$BASE_DIR/./../api_service":/api_service \
--v "$BASE_DIR/./../frontend_react":/frontend_react \
+-v "$BASE_DIR/./../frontend-react":/frontend-react \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e USE_GKE_GCLOUD_AUTH_PLUGIN=True \
 -e WANDB_KEY=/secrets/wandb_key.txt \
